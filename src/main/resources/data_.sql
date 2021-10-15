6ad5b41ed871d096cccfaf43822f7d32bb9eec29
Matheus Leonardo <mtsleonardo.prog@gmail.com>
2021-10-15 00:12:03 -0300

    Commit inicial e implantação do swagger para documentação de API

---
+++ src/main/resources/data_.sql
@@ -0,0 +1,4 @@
+insert into TBL_CURSO (id_curso, codigo_curso, descricao_curso ) values (1, "MIC", "Microservices");
+insert into TBL_CURSO (id_curso, codigo_curso, descricao_curso ) values (2, "ELK", "ElasticSearch/Logstash/Kibana");
+insert into TBL_CURSO (id_curso, codigo_curso, descricao_curso ) values (3, "PYT", "Python");
+insert into TBL_CURSO (id_curso, codigo_curso, descricao_curso ) values (4, "BIZ", "Bizagi");
